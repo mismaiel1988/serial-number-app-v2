750ab4f6e9ac599471af0db2b6900a2a4ce15d71
maya <mayamtrenk@gmail.com>
2025-12-12 13:02:24 -0700

    Make orders dashboard the app home

--- app/routes/app.jsx
+++ app/routes/app.jsx
@@ -1,34 +1,9 @@
-import { Outlet, useLoaderData, useRouteError } from "react-router";
-import { boundary } from "@shopify/shopify-app-react-router/server";
-import { AppProvider } from "@shopify/shopify-app-react-router/react";
-import { authenticate } from "../shopify.server";
+import { redirect } from "@react-router/node";
 
-export const loader = async ({ request }) => {
-  await authenticate.admin(request);
-
-  // eslint-disable-next-line no-undef
-  return { apiKey: process.env.SHOPIFY_API_KEY || "" };
+export const loader = async () => {
+  return redirect("/app/orders");
 };
 
-export default function App() {
-  const { apiKey } = useLoaderData();
-
-  return (
-    <AppProvider embedded apiKey={apiKey}>
-      <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
-      </s-app-nav>
-      <Outlet />
-    </AppProvider>
-  );
+export default function AppIndexRedirect() {
+  return null;
 }
-
-// Shopify needs React Router to catch some thrown responses, so that their headers are included in the response.
-export function ErrorBoundary() {
-  return boundary.error(useRouteError());
-}
-
-export const headers = (headersArgs) => {
-  return boundary.headers(headersArgs);
-};
