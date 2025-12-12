fc5b05dd0ee5fd6057d148a5c3264fc9a0b94a5f
maya <mayamtrenk@gmail.com>
2025-12-12 14:54:38 -0700

    Remove auth login and fix embedded app auth

--- app/routes/auth.$.jsx
+++ app/routes/auth.$.jsx
@@ -2,7 +2,8 @@ import { boundary } from "@shopify/shopify-app-react-router/server";
 import { authenticate } from "../shopify.server";
 
 export const loader = async ({ request }) => {
-  await authenticate.admin(request);
+  await authenticate(request);
+
 
   return null;
 };
